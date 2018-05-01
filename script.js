e516cf1d6ef9ead35498f1b93da422f617f0e542
Ronnie Villarini <Villarini91@gmail.com>
2018-05-01 16:24:15 -0500

    Initialize repository. Add stable blackjack release.

---
+++ script.js
@@ -0,0 +1,242 @@
+//
+// Blackjack
+// by Ronnie Villarini
+//
+
+// DOM variables
+let textArea = document.getElementById('textArea'),
+  newGameButton = document.getElementById('newGame'),
+  hitButton = document.getElementById('hit'),
+  stayButton = document.getElementById('stay');
+
+// Game Variables
+gameStarted = false;
+gameOver = false;
+playerWon = false;
+tie = false;
+dealerCards = [];
+playerCards = [];
+dealerScore = 0;
+playerScore = 0;
+deck = [];
+
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+showStatus();
+
+// Card Array
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = [
+  'Ace',
+  'King',
+  'Queen',
+  'Jack',
+  'Ten',
+  'Nine',
+  'Eight',
+  'Seven',
+  'Six',
+  'Five',
+  'Four',
+  'Three',
+  'Two',
+];
+
+// Game Start
+newGameButton.addEventListener('click', function() {
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false;
+  
+  deck = createDeck();
+  shuffleDeck(deck);
+
+  // Deal cards
+  dealerCards = [getNextCard(), getNextCard()];
+  playerCards = [getNextCard(), getNextCard()];
+  
+  // Adjust button visibility, show status
+  newGameButton.style.display = 'none';
+  hitButton.style.display = 'inline';
+  stayButton.style.display = 'inline';
+  showStatus();
+});
+
+// Hit button
+hitButton.addEventListener('click', function() {
+  playerCards.push(getNextCard());
+  checkForEndOfGame();
+  showStatus();
+});
+
+// Stay button
+stayButton.addEventListener('click', function() {
+  gameOver = true;
+  checkForEndOfGame();
+  showStatus();
+});
+
+// Function for creating a deck
+function createDeck() {
+  let deck = [];
+  for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      let card = {
+        suit: suits[suitIdx],
+        value: values[valueIdx],
+      };
+      deck.push(card);
+    }
+  }
+  return deck;
+}
+
+// Function for shuffling cards
+function shuffleDeck(deck) {
+  for (let i = 0; i < deck.length; i++) {
+    let swapIdx = Math.trunc(Math.random() * deck.length);
+    let tmp = deck[swapIdx];
+    deck[swapIdx] = deck[i];
+    deck[i] = tmp;
+  }
+}
+
+// Get next card
+function getNextCard() {
+  return deck.shift();
+}
+
+// Get card object as string
+function getCardString(card) {
+  return card.value + ' of ' + card.suit;
+}
+
+// Function for showing status
+function showStatus() {
+  if (!gameStarted) {
+    textArea.innerText = 'Welcome to Blackjack!';
+    return;
+  }
+  
+  let dealerCardString = '';
+  for (let i=0; i < dealerCards.length; i++) {
+    dealerCardString += getCardString(dealerCards[i]) + '\n';
+    }
+    
+  let playerCardString = '';
+  for (let i=0; i < playerCards.length; i++) {
+    playerCardString += getCardString(playerCards[i]) + '\n';
+  }
+  
+  updateScores();
+  
+  textArea.innerText = 
+  'Dealer has:\n' +
+  dealerCardString +
+  '(score: ' + dealerScore + ')\n\n' +
+  
+  'Player has:\n' +
+  playerCardString +
+  '(score: ' + playerScore + ')\n\n';
+  
+  if (gameOver) {
+    if (playerWon) {
+      textArea.innerText += "YOU WIN!";
+    }
+    else if (tie) {
+      textArea.innerText += "TIE GAME";
+    }
+    else {
+      textArea.innerText += "DEALER WINS!";
+    }
+    newGameButton.style.display = 'inline';
+    hitButton.style.display = 'none';
+    stayButton.style.display = 'none';
+  }
+}
+
+// Update scores
+function updateScores() {
+  dealerScore = getScore(dealerCards);
+  playerScore = getScore(playerCards);
+}
+
+// Get scores
+function getScore(cardArray) {
+  let score = 0;
+  let hasAce = false;
+  for (let i=0; i < cardArray.length; i++) {
+    let card = cardArray[i];
+    score += getCardNumericValue(card);
+    if (card.value === 'Ace') {
+      hasAce = true;
+    }
+  }
+  if (hasAce && score + 10 <= 21) {
+    return score + 10;
+  }
+  return score;
+}
+
+// Get card numeric value
+function getCardNumericValue(card) {
+  switch(card.value) {
+    case 'Ace':
+      return 1;
+    case 'Two':
+      return 2;
+    case 'Three':
+      return 3;
+    case 'Four':
+      return 4;
+    case 'Five':
+      return 5;
+    case 'Six':
+      return 6;
+    case 'Seven':
+      return 7;
+    case 'Eight':
+      return 8;
+    case 'Nine':
+      return 9;
+    default:
+      return 10;
+  }
+}
+
+// Check for end of game
+function checkForEndOfGame() {
+  updateScores();
+  
+  if (gameOver) {
+    // let dealer take cards
+    while(dealerScore < playerScore
+        && playerScore <= 21
+        && dealerScore <= 21) {
+      dealerCards.push(getNextCard());
+      updateScores();
+        }
+  }
+  
+  if (playerScore > 21) {
+    playerWon = false;
+    gameOver = true;
+  }
+  else if (dealerScore > 21) {
+    playerWon = true;
+    gameOver = true;
+  }
+  else if (gameOver) {
+    
+    if (playerScore > dealerScore) {
+      playerWon = true;
+    }
+    else if (playerScore === dealerScore) {
+      tie = true;
+    }
+    else {
+      playerWon = false;
+    }
+  }
+}
+// Test block
